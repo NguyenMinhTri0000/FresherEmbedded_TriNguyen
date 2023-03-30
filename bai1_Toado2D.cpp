9c6c45c44a1b56c79efdfaa55d5e9075b5f364ca
NMT <nguyenminhtri.20122001@gmail.com>
2023-03-30 14:10:32 +0700

    sua bai 1 xong

--- bai1_Toado2D.cpp
+++ bai1_Toado2D.cpp
@@ -1,211 +1,177 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
-#include <vector>
-#include <string.h>
 
-using namespace std;
-
-typedef enum{
-    ADD =1,
-    DISTANCE,
-    TRIANGLEAREA,
-}CHOICE;
-
-class Coordinate {
-private:
-    char name[2];
-    uint8_t x;
-    uint8_t y;
-public:
-    Coordinate(char name[2], uint8_t x, uint8_t y);
-    uint8_t getX();
-    void setX(uint8_t x);
-    uint8_t getY();
-    void setY(uint8_t y);
-    char *getName();
-    void setName(char l_name[2]);
-
+class Coordinates{
+    private:
+        uint8_t coordinateX;
+        uint8_t coordinateY;
+    public:
+        Coordinates( uint8_t x = 0,  uint8_t y = 0);
+        void setCoordinateX( uint8_t x);
+        void setCoordinateY( uint8_t y);
+        uint8_t getCoordinateX();
+        uint8_t getCoordinateY();
 };
 
-Coordinate::Coordinate(char l_name[2], uint8_t l_x, uint8_t l_y) {
-    x = l_x;
-    y = l_y;
-    strcpy(name, l_name);    
+Coordinates::Coordinates( uint8_t x,  uint8_t y){
+    // printf("%d, %d", x, y);
+    coordinateX = x;
+    coordinateY = y;
 }
 
-uint8_t Coordinate::getX() {
-    return this->x;
+uint8_t Coordinates::getCoordinateX() 
+{
+    return this->coordinateX;
 }
 
-void Coordinate::setX(uint8_t x) {
-    this->x = x;
+uint8_t Coordinates::getCoordinateY() {
+    return this->coordinateY;
 }
 
-uint8_t Coordinate::getY() {
-    return this->y;
+void Coordinates::setCoordinateX(uint8_t x) {
+    coordinateX = x;
 }
 
-void Coordinate::setY(uint8_t y) {
-    this->y = y;
+void Coordinates::setCoordinateY( uint8_t y) {
+    coordinateY = y;
 }
 
-char *Coordinate::getName() {
-    return this->name;
-}
+class SpaceMath{
+    private:
+        Coordinates firstPoint;
+        Coordinates secondPoint;
+        Coordinates thirdPoint;
+        bool isDistance;
+    public:
+        SpaceMath(const Coordinates first, const Coordinates second, const Coordinates third);
+        SpaceMath(const Coordinates first, const Coordinates second);
+        void setfirstPoint(const Coordinates fisrt);
+        void setsecondPoint(const Coordinates second);
+        void setthirdPoint(const Coordinates third);
 
-void Coordinate::setName(char l_name[2]) {
-    strcpy(name, l_name);
-}
-
-class SpatialOperations {
-private:
-    vector <Coordinate>  ListPoint;
-    Coordinate Check(char l_name[2]);
-public:
-    void addPoint();
-    void distance();
-    void triangleArea();
-    void show();
-    SpatialOperations();
+        double distance();
+        double trinagleArea();
 };
- 
-SpatialOperations::SpatialOperations(){
-    do
-    {
-    printf("Press 1 to add point\n");
-    printf("Press 2 to calculate distance\n");
-    printf("Press 3 to calculate triangle area\n");
 
-    uint8_t key = 0;
-    scanf("%hhu", &key);
-
-        switch (key)
-        {
-        case ADD:
-            addPoint();
-            break;        
-        case DISTANCE:
-            distance();
-            break;
-        case TRIANGLEAREA:
-            triangleArea();
-            break;                              
-        default:
-            printf("Your choice: %hhu", &key);
-            printf("Invalid input, please try again.\n");
-            continue;
-        } 
-    } while (true);    
+SpaceMath::SpaceMath(const Coordinates first, const Coordinates second, const Coordinates third)
+{
+    isDistance = false;
+    firstPoint = first;
+    secondPoint = second;
+    thirdPoint = third;
 }
 
-Coordinate SpatialOperations::Check(char l_name[2]){
-    uint8_t key;
-    // do
-    // {
-        for(uint8_t i = 0; i < ListPoint.size(); i++){
-            if(strcmp(ListPoint[i].getName(), l_name)==0){            
-                return ListPoint[i];
-            }  
-            key = 1;
-        }
-    //     if(key == 1) printf("Point %s could not be found. Please re-enter", l_name);
-    // } while (key == 1);
+SpaceMath::SpaceMath(const Coordinates first, const Coordinates second)
+{
+    isDistance = true;
+    firstPoint = first;
+    secondPoint = second;
 }
 
-void SpatialOperations::addPoint(){
-    uint8_t l_x;
-    char l_name[2];
-    uint8_t l_y;
-    uint8_t key;
-
-    do
-    {
-        printf(" ---- ADD POINT ---- \n");
-        printf("Enter point name\n");
-        scanf("%s",l_name);
-        printf("input x coordinates\n");
-        scanf("%hhd", &l_x);
-        printf("input y coordinates\n");
-        scanf("%hhd", &l_y);
-
-        Coordinate point(l_name, l_x, l_y);
-        ListPoint.push_back(point);
-
-        show();
-        printf("Press 1 to continue adding point\n");
-        printf("press 2 to exit");
-        scanf("%hhu", &key);
-    } while (key == 1);
+void SpaceMath::setfirstPoint(const Coordinates first){
+    firstPoint = first;
 }
 
-void SpatialOperations::distance(){
-uint8_t key;
-    do
-    {           
-        char namePoint1[2];
-        char namePoint2[2];   
-
-        printf("enter the first point name\n");
-        scanf("%s", namePoint1);
-        printf("enter the second point name\n");
-        scanf("%s", namePoint2); 
-    
-        double dx = Check(namePoint1).getX() - Check(namePoint2).getX();
-        double dy = Check(namePoint1).getY() - Check(namePoint2).getY();
-        
-        printf("Ket qua: %lf\n", sqrt(dx*dx + dy*dy));
-
-        printf("Press 1 to continue withdraw the book\n");
-        printf("press 2 to exit");
-        scanf("%hhu", &key);        
-    } while (key == 1);
+void SpaceMath::setsecondPoint(const Coordinates second){
+    secondPoint = second;
 }
 
-    void SpatialOperations::triangleArea(){
-    //     char namePoint1[2];
-    //     char namePoint2[2]; 
-    //     char namePoint3[2];           
-
-    //     printf("enter the first point name\n");
-    //     scanf("%s", namePoint1);
-    //     printf("enter the second point name\n");
-    //     scanf("%s", namePoint2); 
-    //     printf("enter the third point name\n");
-    //     scanf("%s", namePoint3);    
-
-    // printf("Ket qua %s", fabs(0.5 * (Check(namePoint1).getX * (Check(namePoint2).getY - Check(namePoint3).y) + Check(namePoint2).getX * (Check(namePoint3).getY - Check(namePoint1).getY) + Check(namePoint3).getX * (Check(namePoint1).getY - Check(namePoint2).getY))));            
-    }
-
-void SpatialOperations::show(){
-    printf(" ---- SHOW ---- \n");
-    if(ListPoint.empty())
-    {
-        printf(" Empty list \n");
+void SpaceMath::setthirdPoint(const Coordinates third)
+{
+    if(!isDistance){
+        thirdPoint = third;
     }
     else{
-        printf("Point\tx\ty\n");
-        for(uint8_t i = 0; i< ListPoint.size(); i++){                                
-            printf("%s\t%hhu\t%hhu\n", ListPoint[i].getName(), ListPoint[i].getX(), ListPoint[i].getY());
-        }
+        printf("Chuong trinh khoang cach chi can 2 diem");
     }
 }
-    // double SpatialOperations::distance(Coordinate c1, Coordinate c2) {
-    //     double dx = c1.getX() - c2.getX();
-    //     double dy = c1.getY() - c2.getY();
-    //     return sqrt(dx*dx + dy*dy);
-    // }
 
-    // double SpatialOperations::triangleArea(Coordinate c1, Coordinate c2, Coordinate c3) {
-    //     double a = distance(c1, c2);
-    //     double b = distance(c2, c3);
-    //     double c = distance(c3, c1);
-    //     double p = (a + b + c) / 2;
-    //     return sqrt(p * (p-a) * (p-b) * (p-c));
-    // }
-int main(int argc, char const *argv[]) {
+double SpaceMath::distance(){
+    double dist;
+    if(isDistance){
+        double dx = firstPoint.getCoordinateX() - secondPoint.getCoordinateX();
+        double dy = firstPoint.getCoordinateY() - secondPoint.getCoordinateY();
+        dist = sqrt(dx*dx + dy*dy);
+    }
+    return dist;
+}
 
-    SpatialOperations sp;
+double SpaceMath::trinagleArea(){
+    double area;
+    if(!isDistance){ 
+        double a = firstPoint.getCoordinateX()*secondPoint.getCoordinateY() + secondPoint.getCoordinateX()*thirdPoint.getCoordinateY() + thirdPoint.getCoordinateX()*firstPoint.getCoordinateY();
+        double b = firstPoint.getCoordinateY()*secondPoint.getCoordinateX() + secondPoint.getCoordinateY()*thirdPoint.getCoordinateX() + thirdPoint.getCoordinateY()*firstPoint.getCoordinateX();
+        area = 0.5 * (a - b);   //Trời ơi để 1/2 làm nó cứ là không hoài
+        if(area<0)  area = area * (-1);    
+    }
+    return area;   
+}
+
+
+int main(int argc, char const *argv[])
+{
+    Coordinates first, second, third;
+    uint8_t temp;
+
+//Đoạn dưới này làm tạm thôi, rãnh làm gọn lại và hợp lí hơn
+    NhapToaDo:
+    printf("Nhap vao toa do X diem thu nhat\n");
+    scanf("%hhd", &temp);
+    first.setCoordinateX(temp);
+    printf("Nhap vao toa do Y diem thu nhat\n");
+    scanf("%hhd", &temp);
+    first.setCoordinateY(temp);     
+
+    printf("Nhap vao toa do X diem thu hai\n");
+    scanf("%hhd", &temp);
+    second.setCoordinateX(temp);
+    printf("Nhap vao toa do Y diem thu hai\n");
+    scanf("%hhd", &temp);
+    second.setCoordinateY(temp); 
+
+    printf("Nhap vao toa do X diem thu ba\n");
+    scanf("%hhd", &temp);
+    third.setCoordinateX(temp);
+    printf("Nhap vao toa do Y diem thu ba\n");
+    scanf("%hhd", &temp);
+    third.setCoordinateY(temp); 
+
+    printf("Diem A: %d, %d\n", first.getCoordinateX(), first.getCoordinateY())   ;
+    printf("Diem B: %d, %d\n", second.getCoordinateX(), second.getCoordinateY())   ;      
+    printf("Diem C: %d, %d\n", third.getCoordinateX(), third.getCoordinateY())   ; 
+
+    do
+    {
+        printf("Nhan 1 de tinh khoang cach tu 2 diem\n");            
+        printf("Nhan 2 de tinh dien tich tam giac\n");
+        printf("Nhan 3 de thay doi toa do\n");
+        uint8_t key;
+        scanf("%hhd", &key);
+
+        // switch (key)
+        // {
+        // case 1:
+        //     SpaceMath a(first, second);
+        //     break;
+        // case 2:
+        //     SpaceMath b(first, second, third);
+        //     break;        
+        // default:
+        //     break;
+        // }
+
+        if(key == 1) {
+            SpaceMath sm(first, second);
+            printf("Khoang cach: %lf\n", sm.distance());
+        }
+        else if(key == 2){
+            SpaceMath sm2(first, second, third);
+            printf("Dien Tich: %lf\n", sm2.trinagleArea());            
+        }
+        else goto NhapToaDo;
+    } while (true);
+    
 
     return 0;
 }
